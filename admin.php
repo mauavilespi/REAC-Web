c82756943ab2dc0e783628cbf5a2c65d7a0a688c
Mauricio Aviles Piña <mauavilespi@E7470-by-TheMAP.local>
2020-09-06 22:55:43 -0500

    admin work (without Ajax)

--- admin.php
+++ admin.php
@@ -9,12 +9,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administraci&oacute;n</title>
     <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
     <!-- Materialize -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <!-- Confirm jQuery -->
     <link rel="stylesheet" href="./plugins/confirm/css/jquery-confirm.css">
     <!-- Validetta -->
     <link rel="stylesheet" href="./plugins/validetta/validetta.min.css">
+    <!-- Icons Materialize -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- JS -->
     <!-- jQuery -->
@@ -35,21 +38,44 @@
 
 <body>
     <header>
-        <img src="./images/Header.png" alt="Contacto Docente" class="responsive-img">
+        <img src="./images/HeaderS.png" alt="Contacto Docente" class="responsive-img">
     </header>
 
-    <form action="./includes/validate.php" method="POST">
-        <h1>Administraci&oacute;n</h1>
-        <label for="username">Username:</label>
-        <br>
-        <input type="text" id="username" name="username">
-        <br>
-        <label for="pass">Password:</label>
-        <br>
-        <input type="password" id="pass" name="pass">
-        <br>
-        <input type="submit" value="Ingresar">
-    </form>
+    <main>
+        <div class="container">
+                <form class="col l12" action="./includes/validate.php" method="POST">
+                
+                    <div class="row">
+                        <h1 class="center-align">Administraci&oacute;n</h1>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col l8 offset-l2">
+                            <i class="material-icons prefix">person_outline</i>
+                            <input type="text" id="username" name="username"> 
+                            <label for="username">Usuario</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col l8 offset-l2">
+                            <i class="material-icons prefix">lock</i>
+                            <input type="password" id="pass" name="pass" >
+                            <label for="pass">Contrase&nacute;a</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col l2 offset-l5">
+                            <button type="submit" class="btn blue darken-2 waves-effect waves-light">
+                                <i class="material-icons right">send</i>Acceder
+                            </button>
+                        </div>
+                    </div>
+
+                </form>
+        </div>
+    </main>
 </body>
 
 </html>
